e751fb186e47a40da2f1417d314f9a35c948e4a0
Jenglish3761 <39132245+Jenglish3761@users.noreply.github.com>
2019-06-02 11:02:17 -0400

    Update pair.js

--- java/pair.js
+++ java/pair.js
@@ -3,11 +3,11 @@ var myService = 0x181C; //user data tag
 var myCharacterisitc;
 
 function scan(){
-  if(!navigator.bluetooth.getAvailability()){
-    document.getElementById("status").innerHTML=('bluetooth service unavailable');
-  }
+  //navigator.bluetooth.getAvailability().then(function(isAvailable){
+  //  document.getElementById("status").innerHTML=('bluetooth service unavailable');
+  //})
   navigator.bluetooth.requestDevice({
-    acceptAllDevices: true, //no filter accept all devices
+    acceptAllDevices: true, //no filter, accept all devices
     optionalServices: [myService] //device serve
   })
   .then(function(device){
