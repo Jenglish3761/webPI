58016d52657b98e0d41ffd5bfed43023e12a3d9b
Jenglish3761 <39132245+Jenglish3761@users.noreply.github.com>
2019-07-13 13:15:13 -0400

    Update BluetoothTerminal.js

--- java/BluetoothTerminal.js
+++ java/BluetoothTerminal.js
@@ -13,7 +13,7 @@ class BluetoothTerminal {
    * @param {string} [receiveSeparator='\n'] - Receive separator
    * @param {string} [sendSeparator='\n'] - Send separator
    */
-  constructor(serviceUuid = 0x6E400001B5A3F393E0A9E50E24DCCA9E, characteristicUuid = 0x0003,
+  constructor(serviceUuid = '6E400001-B5A3-F393-E0A9-E50E24DCCA9E', characteristicUuid = 0x0003,
       receiveSeparator = '\n', sendSeparator = '\n') {
     // Used private variables.
     this._receiveBuffer = ''; // Buffer containing not separated data.
