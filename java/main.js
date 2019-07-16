33ac93085f2fc4f4a248cf20dc258207b17ba2a4
Jenglish3761 <39132245+Jenglish3761@users.noreply.github.com>
2019-07-16 12:07:06 -0400

    Update main.js

--- java/main.js
+++ java/main.js
@@ -108,6 +108,8 @@ var chart = new CanvasJS.Chart("chartContainer", {
 	},
 	axisY: {
 		title: "m/s^2",
+		minimum: -5,
+		maximum: 5
 		//valueFormatString: "#0.",
 		//suffix: "",
 		//stripLines: [{
