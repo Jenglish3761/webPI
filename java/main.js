938ab0bd8c231ee148e8d73765548b65b7464f05
Jenglish3761 <39132245+Jenglish3761@users.noreply.github.com>
2019-07-19 10:46:38 -0400

    Update main.js

--- java/main.js
+++ java/main.js
@@ -10,9 +10,11 @@ const disconnectButton = document.getElementById('disconnect');
 const terminalContainer = document.getElementById('terminal');
 const sendForm = document.getElementById('send-form');
 const inputField = document.getElementById('input');
-var t = 0;
+var t = 0; //data variables
 var ot = 0;
 var za = 0;
+var zv = 0;
+var zp = 0;
 // Helpers.
 const defaultDeviceName = 'Terminal';
 const terminalAutoScrollingLimit= terminalContainer.offsetHeight / 2;
@@ -41,12 +43,12 @@ const terminal = new BluetoothTerminal();
 // Override `receive` method to log incoming data to the terminal.
 terminal.receive = function(data) {
   //logToTerminal(data, 'in'); //show data in terminal
-  //alert(message);
   sm = data.split(",");
-	t = sm[3]/1000;
-	za = sm[2];
-  //document.cookie = "t=" + sm[3] + "; z=" + sm[2];
-  console.log(za);
+	t = sm[0]/1000;
+	za = sm[1];
+	zv = sm[2];
+	zp = sm[3];
+
   //'data',JSON.stringify({t: sm[3], z: sm[2]}));
   //console.log(JSON.parse(document.cookie('data')));
 };
@@ -82,15 +84,6 @@ disconnectButton.addEventListener('click', () => {
   deviceNameLabel.textContent = defaultDeviceName;
 });
 
-//sendForm.addEventListener('submit', (event) => {
-//  event.preventDefault();
-
-//  send(inputField.value);
-
-// inputField.value = '';
-//  inputField.focus();
-//});
-
 // Switch terminal auto scrolling if it scrolls out of bottom.
 terminalContainer.addEventListener('scroll', () => {
   const scrollTopOffset = terminalContainer.scrollHeight -
@@ -100,41 +93,32 @@ terminalContainer.addEventListener('scroll', () => {
 });
 
 window.onload = function () {
-var dataPoints = [{x: 0, y: 0}];
-var chart = new CanvasJS.Chart("chartContainer", {
-	animationEnabled: true,  
-	title:{
-		text: "Z-Acceleration"
-	},
-	axisY: {
-		title: "m/s^2",
-		//minimum: -5,
-		//maximum: 5
-		//valueFormatString: "#0.",
-		//suffix: "",
-		//stripLines: [{
-		//	value: 3366500,
-		//	label: "Average"
-		//}]
-	},
-	data: [{
-		//yValueFormatString: "#.### m/s^2",
-		//xValueFormatString: "##.#",
-		type: "line",
-		dataPoints: dataPoints
-	}]
-});
-chart.render();
+	var dataPoints = [{x: 0, y: 0}];
+	var aChart = new CanvasJS.Chart("aChart", {
+		animationEnabled: true,  
+		title:{
+			text: "Z-Acceleration"
+		},
+		axisY: {
+			title: "m/s^2",
+		
+		},
+		data: [{
+			type: "line",
+			dataPoints: dataPoints
+		}]
+	});
+	aChart.render();
 	var updateChart= function(){
 		console.log(ot);
 		console.log(t);
 		console.log(za);
 		var az = za;
-		if (ot != t && t != NaN){
-			dataPoints.push({x: t, y: Number(za)});
+		if (ot != t && ot != NaN){
+			dataPoints.push({x: ot, y: Number(za)});
 			//chart.data[0].addTo("dataPoints", {x: t, y: za});
-			chart.render();
-			chart.options.title.text = za;
+			aChart.render();
+			aChart.options.title.text = za;
 		}
 		ot = t;
 	};
